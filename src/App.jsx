5813222226b47fa0cb77e7f78cc2967d7ea6569b
Raheel Ali 023-22-0024 <raheelali.bscsf22@iba-suk.edu.pk>
2025-03-25 09:31:55 +0500

    add baseline and css design for countdown and light switch

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import CountdownLightSwitch from './assets/CountdownLightSwitch'
+function App() {
+  return (
+    <>
+     <CountdownLightSwitch />
+    </>
+  )
+}
+
+export default App
